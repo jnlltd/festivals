ff8e90620ef0a05a4338abb573fafc18598e034d
domurtag <domurtag@users.noreply.github.com>
2015-05-18 23:53:02 +0100

    removed obsolete migration script

--- scripts/migrate-pac4j.sql
+++
@@ -1,4 +0,0 @@
-alter table user change preferred_username social_id varchar(191);
-
-update user set social_login_provider = lower(social_login_provider);
-update user set social_login_provider = 'yahoo' where social_login_provider = 'yahoo!';
